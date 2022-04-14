2cc50a3e16c6196cdfa096cf0ddb6719dc72f259
Mahathi <mathreya24@amherst.edu>
2022-04-13 22:45:56 -0400

    working on string rn

--- src/MyDS.java
+++ src/MyDS.java
@@ -6,18 +6,23 @@ class Node {
     }
 }
 
-//String
 
 public class MyDS extends OrderedCollection{
     Node end;
+    static int count = 0;
 
     public MyDS() {
         end = null;
     }
+
     public void append(int toAppend){
         Node appNode = new Node(toAppend);
         appNode.prev = end;
         end = appNode;
+        count++;
+        if (count >= 6) {
+            stringMessage();
+        }
     }
     public int peek(){
         if (end == null) {
@@ -25,14 +30,20 @@ public class MyDS extends OrderedCollection{
         }
         return end.num;
     }
+
     public int pop(){
         if (end == null) {
             return 0;
         }
         int fin = end.num;
         end = end.prev;
+        count--;
+        if (count >= 6) {
+            stringMessage();
+        }
         return fin;
     }
+
     public String toString(){
         String s = "";
         Node n = end;
@@ -51,6 +62,18 @@ public class MyDS extends OrderedCollection{
             n = n.prev;
         }
         return count;
+        //could also create a member variable that increments with append and goes down with pop
+    }
+
+    public void stringMessage() {
+        Node n = end;
+
+        for (int i = 0; i < length()-6; i++) {
+            for (int j = 0; j < 6; j++) {
+
+            }
+
+        }
     }
 
 
